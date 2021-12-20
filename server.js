c578b2de37f963377c70ac887512c3c6850a02ec
pytyagi <pytyagi@gmail.com>
2021-12-20 18:57:06 +0530

    Typo update

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
-const PORT = process.env.process || 5050;
+const PORT = process.env.PORT || 5050;
 
 app.listen(PORT, () => {
   console.log(`Server starting on port ${PORT}`);
