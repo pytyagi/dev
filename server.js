f43397956e0946f12af6d9f4afa77a9b2031cdd0
pytyagi <pytyagi@gmail.com>
2022-02-15 17:07:17 +0530

    fix

--- server.js
+++ server.js
@@ -6,17 +6,28 @@ const app = express();
 // Connect to Database
 connectDB();
 
-app.use(express.json({ extended: false }));
-app.get("/", (req, res) => {
-   console.log("/ dashboard route");
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-     console.log("/ Prod dashboard route");
-      res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
-  }
-});
+app.use(express.json());
+// app.get("/", (req, res) => {
+   
+//   if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'));
+//     app.get('*', (req, res) => {
+//      console.log("/ Prod dashboard route");
+//       res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+//     })
+//   }
+// });
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 
 // Define Routes(Middleware)
 app.use("/api/users", require("./routes/api/users"));
