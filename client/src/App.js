665f1bfa954dad0ca3016d35894a79715f261ce9
pytyagi <pytyagi@gmail.com>
2022-02-15 17:36:06 +0530

    Added not found page

--- client/src/App.js
+++ client/src/App.js
@@ -22,56 +22,83 @@ import Profiles from "../src/components/profiles/Profiles";
 import Profile from "../src/components/profile/Profile";
 import Posts from "../src/components/posts/Posts";
 import Post from "../src/components/post/Post";
+import { LOGOUT } from "./actions/types";
+import NotFound from "./components/layout/NotFound";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
 }
 const App = () => {
-  useEffect(() => {
-    store.dispatch(loadUser());
-  }, []);
+ useEffect(() => {
+     // check for token in LS when app first runs
+     if (localStorage.token) {
+         // if there is a token set axios headers for all requests
+         setAuthToken(localStorage.token);
+     }
+     // try to fetch a user, if no token or invalid token we
+     // will get a 401 response from our API
+     store.dispatch(loadUser());
+
+     // log user out from all tabs if they log out in one tab
+     window.addEventListener("storage", () => {
+         if (!localStorage.token) store.dispatch({ type: LOGOUT });
+     });
+ }, []);
 
   return (
-    <Provider store={store}>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <section className="container">
-            <Alert />
-            <Switch>
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/profiles" component={Profiles} />
-              <Route exact path="/profile/:id" component={Profile} />
-              <PrivateRoute exact path="/dashboard" component={Dashboard} />
-              <PrivateRoute
-                exact
-                path="/create-profile"
-                component={CreateProfile}
-              />
-              <PrivateRoute
-                exact
-                path="/edit-profile"
-                component={EditProfile}
-              />
-              <PrivateRoute
-                exact
-                path="/add-experience"
-                component={AddExperience}
-              />
-              <PrivateRoute
-                exact
-                path="/add-education"
-                component={AddEducation}
-              />
-              <PrivateRoute exact path="/posts" component={Posts} />
-              <PrivateRoute exact path="/posts/:id" component={Post} />
-            </Switch>
-          </section>
-        </Fragment>
-      </Router>
-    </Provider>
+      <Provider store={store}>
+          <Router>
+              <Fragment>
+                  <Navbar />
+                  <Route exact path="/" component={Landing} />
+                  <section className="container">
+                      <Alert />
+                      <Switch>
+                          <Route exact path="/register" component={Register} />
+                          <Route exact path="/login" component={Login} />
+                          <Route exact path="/profiles" component={Profiles} />
+                          <Route
+                              exact
+                              path="/profile/:id"
+                              component={Profile}
+                          />
+                          <PrivateRoute
+                              exact
+                              path="/dashboard"
+                              component={Dashboard}
+                          />
+                          <PrivateRoute
+                              exact
+                              path="/create-profile"
+                              component={CreateProfile}
+                          />
+                          <PrivateRoute
+                              exact
+                              path="/edit-profile"
+                              component={EditProfile}
+                          />
+                          <PrivateRoute
+                              exact
+                              path="/add-experience"
+                              component={AddExperience}
+                          />
+                          <PrivateRoute
+                              exact
+                              path="/add-education"
+                              component={AddEducation}
+                          />
+                          <PrivateRoute exact path="/posts" component={Posts} />
+                          <PrivateRoute
+                              exact
+                              path="/posts/:id"
+                              component={Post}
+                          />
+                          <Route path="/*" element={<NotFound />} />
+                      </Switch>
+                  </section>
+              </Fragment>
+          </Router>
+      </Provider>
   );
 };
 export default App;
